4ea257185d03bee9aedfb2e2721bf99bc5983073
Charlie Fish <contact@charlie.fish>
2024-01-11 16:19:22 -0700

    Fixing filtering of events

---
+++ src/utils/filtersMatchEvent.test.ts
@@ -0,0 +1,36 @@
+import { filtersMatchEvent } from "./filtersMatchEvent";
+
+test.concurrent.each([
+	[
+		{
+			"kinds": [1],
+			"authors": ["d77637850017cffa7a61c7032db0f28be947d5487f9d504aabe4449a91b53cff"]
+		},
+		{
+			"id": "c9c8eea07db297ab0bee9e60bf1cb40ba13f93eba4ac50797e3aa5e3afe4c80f",
+			"pubkey": "9887797d06372fa7aa79950328e0754277ee748efa2222204c713ac03f1a5a81",
+			"created_at": 1703779327,
+			"kind": 1,
+			"tags": [],
+			"content": "Random content"
+		},
+		false
+	],
+	[
+		{
+			"kinds": [1],
+			"authors": ["9887797d06372fa7aa79950328e0754277ee748efa2222204c713ac03f1a5a81"]
+		},
+		{
+			"id": "c9c8eea07db297ab0bee9e60bf1cb40ba13f93eba4ac50797e3aa5e3afe4c80f",
+			"pubkey": "9887797d06372fa7aa79950328e0754277ee748efa2222204c713ac03f1a5a81",
+			"created_at": 1703779327,
+			"kind": 1,
+			"tags": [],
+			"content": "Random content"
+		},
+		true
+	],
+])(`filtersMatchEvent()`, async (filters, event, expected) => {
+	expect(filtersMatchEvent(filters, event as any)).toBe(expected);
+});
--- src/utils/filtersMatchEvent.ts
+++ src/utils/filtersMatchEvent.ts
@@ -2,20 +2,20 @@ import { FiltersObject } from "../types/FiltersObject";
 import { Event } from "../types/Event";
 
 /**
- * Returns true if the event matches any of the filters.
+ * Returns true if the event matches the filters.
  */
 export function filtersMatchEvent(filters: FiltersObject, event: Event): boolean {
 	if (Object.keys(filters).length === 0) {
 		return true;
 	}
 
-	const idFilterPass = filters.ids && filters.ids.includes(event.id);
-	const authorFilterPass = filters.authors && filters.authors.includes(event.pubkey);
-	const kindFilterPass = filters.kinds && filters.kinds.includes(event.kind);
-	const sinceFilterPass = filters.since && filters.since <= event.created_at;
-	const untilFilterPass = filters.until && filters.until >= event.created_at;
+	const idFilterPass = !filters.ids || filters.ids.includes(event.id);
+	const authorFilterPass = !filters.authors || filters.authors.includes(event.pubkey);
+	const kindFilterPass = !filters.kinds || filters.kinds.includes(event.kind);
+	const sinceFilterPass = !filters.since || filters.since <= event.created_at;
+	const untilFilterPass = !filters.until || filters.until >= event.created_at;
 
-	return Boolean(idFilterPass || authorFilterPass || kindFilterPass || sinceFilterPass || untilFilterPass);
+	return Boolean(idFilterPass && authorFilterPass && kindFilterPass && sinceFilterPass && untilFilterPass);
 
 	// @TODO: implement "#<single-letter (a-zA-Z)>"
 }
