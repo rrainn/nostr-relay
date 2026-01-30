fc2cbcaa40a6e9c45a0e5a506ebf1728a77af2ab
Charlie Fish <contact@charlie.fish>
2026-01-30 13:23:04 -0700

    Adding better documentation

--- src/types/EventKind.ts
+++ src/types/EventKind.ts
@@ -6,8 +6,20 @@ export enum EventKind {
 
 export enum EventKindType {
 	unknown = 0,
+	/**
+	 * Event expected to be stored by relays.
+	 */
 	regular = 1,
+	/**
+	 * Only the latest event for each combination of `pubkey` and `kind` is expected to be stored by relays. Older events may be discarded.
+	 */
 	replaceable = 2,
+	/**
+	 * Event not expected to be stored by relays.
+	 */
 	ephemeral = 3,
+	/**
+	 * Event is addressable by their `kind`, `pubkey` and the value of their `d` tag. Only the latest event for each combination of those values should be stored by relays. Older events may be discarded.
+	 */
 	parameterized_replaceable = 4,
 }
